f140043b965436026001ac91c0de8d3973e9d75b
dvrepa <d.v.repnikov@gmail.com>
2022-07-14 21:25:38 +0300

    Почистил от лишнего

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { LayoutComponent } from "components/layout/layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <LayoutComponent>
-      <Component {...pageProps} />
-    </LayoutComponent>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,12 @@
 import { NextPage } from "next";
-import { getFeaturedEvents } from "dummy-data";
-import { EventListComponent } from "components/events/event-list.component";
 
 const HomePage: NextPage = () => {
-  const featuredEvents = getFeaturedEvents();
-
   return (
-    <div>
-      <EventListComponent items={featuredEvents} />
-    </div>
+    <ul>
+      <li>Product 1</li>
+      <li>Product 2</li>
+      <li>Product 3</li>
+    </ul>
   );
 };
 
