2886721bfbaafd44074aca0134016a316b6fcd54
dvrepa <d.v.repnikov@gmail.com>
2022-07-14 23:12:59 +0300

    Разобрался с генерацией статики

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,35 @@
-import { NextPage } from "next";
+import path from "path";
+import fs from "fs/promises";
+import { GetStaticProps, NextPage } from "next";
 
-const HomePage: NextPage = () => {
+interface IHomePageProps {
+  products: Array<{ id: string; title: string }>;
+}
+
+const HomePage: NextPage<IHomePageProps> = ({ products }) => {
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
+};
+
+export const getStaticProps: GetStaticProps<IHomePageProps> = async () => {
+  const result = await fs.readFile(
+    path.join(process.cwd(), "data", "dummy-backend.json"),
+    { encoding: "utf-8" }
+  );
+
+  const parsedData = JSON.parse(result);
+
+  return {
+    props: {
+      products: parsedData.products,
+    },
+    revalidate: 60,
+  };
 };
 
 export default HomePage;
